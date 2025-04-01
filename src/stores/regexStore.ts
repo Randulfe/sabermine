f2e5ab57d4cfa7f1cb7d97fb00a0c7bd3d22c416
Mateo Randulfe <mateorandulfe@gmail.com>
2025-04-01 20:09:55 +0100

    feat: core functionality for showing matches

--- src/stores/regexStore.ts
+++ src/stores/regexStore.ts
@@ -4,15 +4,21 @@ import type { IRegex } from "@/entities/regex";
 
 interface RegexStore {
   regexes: IRegex[];
+  selectedId: string | null;
+  isLoading: boolean;
   addRegex: (value: string) => void;
   removeRegex: (id: string) => void;
   setRegex: (regex: Pick<IRegex, "id"> & Partial<Omit<IRegex, "id">>) => void;
+  setSelectedRegex: (id: string) => void;
+  setLoaded: () => void;
 }
 
 export const useRegexStore = create<RegexStore>()(
   persist(
     (set) => ({
       regexes: [],
+      selectedId: null,
+      isLoading: true,
       addRegex: (value: string) =>
         set((state) => ({
           regexes: [
@@ -23,15 +29,33 @@ export const useRegexStore = create<RegexStore>()(
       removeRegex: (id: string) =>
         set((state) => ({
           regexes: state.regexes.filter((item) => item.id !== id),
+          selectedId: state.selectedId === id ? null : state.selectedId,
         })),
       setRegex: (regex: Pick<IRegex, "id"> & Partial<Omit<IRegex, "id">>) => {
         set((state) => ({
           regexes: state.regexes.map((item) =>
-            item.id === regex.id ? { ...item, ...regex } : item,
+            item.id === regex.id
+              ? {
+                  ...item,
+                  ...regex,
+                  ...(regex.value ? { isApproved: false } : {}),
+                }
+              : item,
           ),
         }));
       },
+      setSelectedRegex: (id: string) => {
+        set(() => ({ selectedId: id }));
+      },
+      setLoaded: () => set({ isLoading: false }),
     }),
-    { name: "regexStore" },
+    {
+      name: "regexStore",
+      onRehydrateStorage: () => (state) => {
+        if (state) {
+          state.setLoaded();
+        }
+      },
+    },
   ),
 );
