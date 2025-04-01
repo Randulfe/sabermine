198e64f59452d4a1ff84f10fd19a6036a85085a3
Mateo Randulfe <mateorandulfe@gmail.com>
2025-04-01 08:13:38 +0100

    fix: use tw/postcss

--- postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-import tailwindcssPostcss from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
-
-const config = {
-  plugins: [tailwindcssPostcss, autoprefixer],
-};
-
-export default config;
